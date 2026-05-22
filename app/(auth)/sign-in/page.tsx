875aaff337895e1db6bb8ce2445f91566044da2b
hasuhasmath <hasuh5pepe@gmail.com>
2026-05-22 15:48:00 +0530

    Implement Auth UI & Layout

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,59 @@
+'use client'
+import {useForm} from "react-hook-form";
+import {Button} from "@/components/ui/button";
+import InputField from "@/components/forms/InputField";
+import FooterLinks from "@/components/forms/FooterLinks";
+
+const SignIn = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<SignInFormData>({
+        defaultValues : {
+            email :'',
+            password : '',
+        },
+        mode: 'onBlur'
+    })
+    const onSubmit = async (data: SignInFormData) => {
+        try {
+            console.log(data)
+        }catch (e){
+            console.log(e)
+        }
+    }
+
+    return (
+        <>
+            <h1 className="form-title">Welcome Back</h1>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+                <InputField
+                    name="email"
+                    label="Email"
+                    placeholder="abc@gmail.com"
+                    register={register}
+                    error={errors.email}
+                    validation={{required:'Email is required', pattern:'/^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/'}}
+                />
+
+                <InputField
+                    name='password'
+                    label='Password'
+                    placeholder={"Enter your Password"}
+                    type="password"
+                    register={register}
+                    error={errors.password}
+                    validation={{required:'Password is required'}}
+                />
+
+                <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
+                    {isSubmitting ? 'Signing In...' : 'Sign In'}
+                </Button>
+
+                <FooterLinks text="Don't have an account?" linkText="Sign Up" href="/sign-up"/>
+            </form>
+        </>
+    )
+}
+export default SignIn
